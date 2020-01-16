159a859b161e608d3ae6a2162d08c7435d313841
Steven Black <steveb@stevenblack.com>
2020-01-16 12:12:23 -0500

    Move the standings type definition to the standings file.

--- nhl.standings.go
+++ nhl.standings.go
@@ -10,6 +10,80 @@ import (
 	"time"
 )
 
+type (
+	nhl_standings struct {
+		Copyright string `json:"copyright"`
+		Records   []struct {
+			StandingsType string `json:"standingsType"`
+			League        struct {
+				ID   int    `json:"id"`
+				Name string `json:"name"`
+				Link string `json:"link"`
+			} `json:"league"`
+			Division struct {
+				ID           int    `json:"id"`
+				Name         string `json:"name"`
+				NameShort    string `json:"nameShort"`
+				Link         string `json:"link"`
+				Abbreviation string `json:"abbreviation"`
+			} `json:"division"`
+			Conference struct {
+				ID   int    `json:"id"`
+				Name string `json:"name"`
+				Link string `json:"link"`
+			} `json:"conference"`
+			TeamRecords []struct {
+				Team struct {
+					ID   int    `json:"id"`
+					Name string `json:"name"`
+					Link string `json:"link"`
+				} `json:"team"`
+				LeagueRecord struct {
+					Wins   int    `json:"wins"`
+					Losses int    `json:"losses"`
+					Ot     int    `json:"ot"`
+					Type   string `json:"type"`
+				} `json:"leagueRecord"`
+				GoalsAgainst   int    `json:"goalsAgainst"`
+				GoalsScored    int    `json:"goalsScored"`
+				Points         int    `json:"points"`
+				DivisionRank   string `json:"divisionRank"`
+				ConferenceRank string `json:"conferenceRank"`
+				LeagueRank     string `json:"leagueRank"`
+				WildCardRank   string `json:"wildCardRank"`
+				Row            int    `json:"row"`
+				GamesPlayed    int    `json:"gamesPlayed"`
+				Streak         struct {
+					StreakType   string `json:"streakType"`
+					StreakNumber int    `json:"streakNumber"`
+					StreakCode   string `json:"streakCode"`
+				} `json:"streak"`
+				Records struct {
+					DivisionRecords []struct {
+						Wins   int    `json:"wins"`
+						Losses int    `json:"losses"`
+						Ot     int    `json:"ot"`
+						Type   string `json:"type"`
+					} `json:"divisionRecords"`
+					OverallRecords []struct {
+						Wins   int    `json:"wins"`
+						Losses int    `json:"losses"`
+						Ot     int    `json:"ot,omitempty"`
+						Type   string `json:"type"`
+					} `json:"overallRecords"`
+					ConferenceRecords []struct {
+						Wins   int    `json:"wins"`
+						Losses int    `json:"losses"`
+						Ot     int    `json:"ot"`
+						Type   string `json:"type"`
+					} `json:"conferenceRecords"`
+				} `json:"records"`
+				LastUpdated time.Time `json:"lastUpdated"`
+			} `json:"teamRecords"`
+		} `json:"records"`
+	}
+)
+
 func standings() {
 	url := urls["standings"]
 
@@ -34,7 +108,7 @@ func standings() {
 		fmt.Print(err)
 	}
 
-	var stats statistics
+	var stats nhl_standings
 	err = json.Unmarshal(data, &stats)
 	if err != nil {
 		fmt.Println("error:", err)
